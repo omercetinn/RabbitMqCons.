f42b238603bb27692a3828e5999b28110bf4b8e0
ömer <mer@DESKTOP-6S1VIE5>
2022-09-28 12:30:36 +0300

    Add project files.

---
+++ RabbitMqCons/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+namespace RabbitMqCons
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"Welcome {message}");
+                };
+                channel.BasicConsume(queue: "NameQueue",
+                    autoAck: true,
+                    consumer: consumer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
